b60be1a850cae3cea829e18bfaab7248541a6108
pccomponentshenry <rafael84barreiro@gmail.com>
2023-01-24 01:25:51 -0300

    start form joinUs

--- src/components/JoinForm/JoinForm.jsx
+++ src/components/JoinForm/JoinForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import s from "../JoinForm/JoinForm.module.css"
 
 export default function JoinForm() {
 
@@ -8,8 +9,46 @@ return(
             <h2> Quiero unirme a Fedeh como voluntario!</h2>
             <h5> Los campos marcados con * son obligatorios</h5>
         </div>
-        <div>
-            <span>aqui va el formulario</span>
+        <div className={s.containerForm} >
+            <div>
+                <form>
+                    <div>
+                        <h5>Nombre *</h5>
+                        <input/>
+                    </div>
+                    <div>
+                        <h5>Edad *</h5>
+                        <input
+                        type="number"
+                        placeholder="Debes ser mayor de 18 años"
+                        />
+                    </div>
+                    <div>
+                        <h5>Email *</h5>
+                        <input/>
+                    </div>
+                    <div>
+                        <h5>Telefono</h5>
+                        <input></input>
+                    </div>
+                   
+                    <div >
+            <button
+              type="submit"
+            >
+              Enviar
+            </button>
+          </div>
+                </form>
+                
+
+            </div>
+            <div>
+            <img src="https://res.cloudinary.com/dbtekd33p/image/upload/v1671422387/cqws5x8n/enyl4rknrg9ub2vtpmb6.png"
+              alt=""
+              className={s.logo} 
+              />
+            </div>
         </div>
 
     </div>
