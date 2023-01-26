4b762449a34c500ab0a21de18fb4e274a96947db
Richard <rsebaburgues@gmail.com>
2023-01-26 18:38:02 +0000

    update estilos

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import N from "./NavBar/NavBar.module.css"
 
 
 export const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <button  onClick={() => loginWithRedirect()}>
+    <button className={N.botonLogin}  onClick={() => loginWithRedirect()}>
       Login
     </button>
   );
