a2355adc89e4e3cacee0ff16dc3191001dd43365
pccomponentshenry <rafael84barreiro@gmail.com>
2023-02-05 20:20:52 -0300

    j

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -7,7 +7,8 @@ import {
     GET_USERS
 } from "../actions/actionName"
 
-const URL = "http://localhost:3001";
+//const URL = "http://localhost:3001";
+const URL = "https://fundacionfedehback-production.up.railway.app/"
 
 export async function populateDB() {
     await axios.get(`${URL}/populateDB`);
