c278c7840a3e33bc8c9526b6f8d41277af7863ae
pccomponentshenry <rafael84barreiro@gmail.com>
2023-03-21 22:06:01 -0300

    add mision in home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -20,6 +20,22 @@ export default function Home() {
 
               <Articles />
             </div> */}
+          <div className={H.containerMission} >
+            <div className={H.mission} >
+              <div className={H.submission} >
+                <span className={H.title} >Misión</span>
+                <p className={H.description} >Ser partícipes de la transformación del tradicional modelo de donación de sangre</p>
+              </div>
+              <div className={H.submission}>
+                <span className={H.title}>Visión</span>
+                <p className={H.description} >Generar un lugar de excelencia en el país </p>
+              </div>
+              <div className={H.submission}>
+                <span className={H.title}>Valores</span>
+                <p className={H.description} >Apoyo integral del paciente y su familia, a través de la contención emocional </p>
+              </div>
+            </div>
+          </div>
           <div className={H.history} >
             <AllEvents />
           </div>
