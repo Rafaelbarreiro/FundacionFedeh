7f761d6d956de197a7c86b6047ce77bc71f0455c
Juan Jose Bianchi <juan-josebianchi@hotmail.com>
2023-06-24 20:39:01 -0300

    todo los eventos

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -11,22 +11,18 @@ export default function Event(props) {
     return (
         <div className={s.main}>
             <div className={s.container}>
-                <h2>{newDate} </h2>
+                <h2 className="container d-flex justify-content-center bg-dark rounded pb-4">{newDate}</h2>
                 <img src={props.img} alt="" className={s.img} />
-                <div className={s.subitem} >
+                <div className={s.subitem}>
                     <h2 className={s.title}>{props.title}</h2>
-                    <h3>{props.subtitle} </h3>
-                    <span>{description[0].substr(0, 90) + "..."} </span>
+                    <h3>{props.subtitle}</h3>
+                    <span>{description[0].substr(0, 90) + "..."}</span>
                     <Link to={`/event/${props._id}`} className={s.imgContainer}>
-                        <button>Ver Más</button>
+                        <button className={s.boton}>Ver Más</button>
                     </Link>
                 </div>
-                <div>
-
-
-                </div>
+                <div></div>
             </div>
-
         </div>
     )
 }
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -8,7 +8,7 @@ import Carousel from "../../components/Carrousel";
 import H from "../Home/Home.module.css";
 import Event from "../../components/Event/Event";
 import mujeres from '../../Images/mujeresFedeh.jpg'
-
+import eventos from '../../Images/eventosfedeh.png'
 
 
 export default function Home() {
@@ -20,7 +20,7 @@ export default function Home() {
   useEffect(() => {
     dispatch(getEvents())
   }, [dispatch])
-  const lastEvents = events.slice(events.length-3)
+  const lastEvents = events.slice(events.length - 3)
 
   useEffect(() => {
     setRenderizar(
@@ -36,7 +36,7 @@ export default function Home() {
         />
       ))
     )
-  }, [events]); 
+  }, [events]);
 
 
   return (
@@ -45,19 +45,19 @@ export default function Home() {
       <hr className={H.hrr} />
       <div className='container-fluid w-100 d-flex flex-wrap justify-content-center pt-4'>
         <span className={H.zoom}>
-          <div className='container mb-4' style={{ background: '#E57373', borderRadius: '10px', paddingBottom:'15px' }}>
+          <div className='container mb-4' style={{ background: '#E57373', borderRadius: '10px', paddingBottom: '15px' }}>
             <h1 className={H.title}>Misión</h1>
             <p className={H.description}>Nuestra misión es establecer y mantener un Centro Especializado y de excelencia en Tucumán, dedicado al diagnóstico y tratamiento de las enfermedades hematooncológicas. Nos comprometemos a ofrecer atención médica de la más alta calidad, utilizando tecnologías de vanguardia y enfoques terapéuticos avanzados.</p>
           </div>
         </span>
         <span className={H.zoom}>
-          <div className='container mb-4' style={{ background: '#64B5F6', borderRadius: '10px',paddingBottom:'15px' }}>
+          <div className='container mb-4' style={{ background: '#64B5F6', borderRadius: '10px', paddingBottom: '15px' }}>
             <h1 className={H.title}>Visión</h1>
             <p className={H.description}>Nuestra visión es convertirnos en un referente en la atención hematooncológica en Tucumán, brindando servicios de calidad que eviten la necesidad de trasladarse a centros médicos en la Capital Federal y así reducir la angustia del desarraigo para los pacientes y sus familias.</p>
           </div>
         </span>
         <span className={H.zoom}>
-          <div className='container mb-4' style={{ background: '#81C784', borderRadius: '10px',paddingBottom:'15px' }}>
+          <div className='container mb-4' style={{ background: '#81C784', borderRadius: '10px', paddingBottom: '15px' }}>
             <h1 className={H.title}>Valores</h1>
             <p className={H.description}>En nuestro centro, nos regimos por un conjunto de valores fundamentales que guían nuestra labor diaria Solidaridad: Nos comprometemos a brindar apoyo y atención compasiva a nuestros pacientes y sus familias. Nos preocupamos por su bienestar físico, emocional y social, y nos esforzamos por crear un ambiente de confianza y apoyo mutuo.</p>
           </div>
@@ -72,21 +72,26 @@ export default function Home() {
       </div>
 
       {/* EVENTOS  */}
-      <div className={H.containerEvents} >
-        <h2>Nuestros Eventos</h2>
-        <div className={H.grid} >
-          {renderizar}
+      <div className={H.containerEvents}>
+        <div className={H.eventos}>
+          <img src={eventos} alt="eventos" style={{ width: '100%', height: '100%' }} />
         </div>
-
-        <div>
-          <p>Enterate de todos los eventos para unir toda la comunidad de FEDEH</p>
-          <Link to={`/events`} >
-            <button>
-              Ingresar a todos los eventos
-            </button>
-          </Link>
+        <div className={H.grid}>
+          <div className={H.eventosLista}>
+            {renderizar}
+          </div>
         </div>
       </div>
+
+      <div className={H.centerContent}>
+        <p>Enterate de todos los eventos para unir toda la comunidad de FEDEH</p>
+        <Link to={`/events`}>
+          <button className={H.boton}>
+            Ingresar a todos los eventos
+          </button>
+        </Link>
+      </div>
+
     </div>
   );
 }
