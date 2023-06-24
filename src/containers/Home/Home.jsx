d519184f31112fc940d0abb9e779a6097c1680b9
Juan Jose Bianchi <juan-josebianchi@hotmail.com>
2023-06-24 16:44:25 -0300

    cambios

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
   }, [dispatch])
   const lastEvents = events.slice(events.length-3)
 
-  /* useEffect(() => {
+  useEffect(() => {
     setRenderizar(
       lastEvents?.reverse().map(el => (
         <Event
@@ -36,7 +36,7 @@ export default function Home() {
         />
       ))
     )
-  }, [events]); */
+  }, [events]); 
 
 
   return (
@@ -45,19 +45,19 @@ export default function Home() {
       <hr className={H.hrr} />
       <div className='container-fluid w-100 d-flex flex-wrap justify-content-center pt-4'>
         <span className={H.zoom}>
-          <div className='container mb-4' style={{ background: '#E57373', borderRadius: '10px' }}>
+          <div className='container mb-4' style={{ background: '#E57373', borderRadius: '10px', paddingBottom:'15px' }}>
             <h1 className={H.title}>Misión</h1>
             <p className={H.description}>Nuestra misión es establecer y mantener un Centro Especializado y de excelencia en Tucumán, dedicado al diagnóstico y tratamiento de las enfermedades hematooncológicas. Nos comprometemos a ofrecer atención médica de la más alta calidad, utilizando tecnologías de vanguardia y enfoques terapéuticos avanzados.</p>
           </div>
         </span>
         <span className={H.zoom}>
-          <div className='container mb-4' style={{ background: '#64B5F6', borderRadius: '10px' }}>
+          <div className='container mb-4' style={{ background: '#64B5F6', borderRadius: '10px',paddingBottom:'15px' }}>
             <h1 className={H.title}>Visión</h1>
             <p className={H.description}>Nuestra visión es convertirnos en un referente en la atención hematooncológica en Tucumán, brindando servicios de calidad que eviten la necesidad de trasladarse a centros médicos en la Capital Federal y así reducir la angustia del desarraigo para los pacientes y sus familias.</p>
           </div>
         </span>
         <span className={H.zoom}>
-          <div className='container mb-4' style={{ background: '#81C784', borderRadius: '10px' }}>
+          <div className='container mb-4' style={{ background: '#81C784', borderRadius: '10px',paddingBottom:'15px' }}>
             <h1 className={H.title}>Valores</h1>
             <p className={H.description}>En nuestro centro, nos regimos por un conjunto de valores fundamentales que guían nuestra labor diaria Solidaridad: Nos comprometemos a brindar apoyo y atención compasiva a nuestros pacientes y sus familias. Nos preocupamos por su bienestar físico, emocional y social, y nos esforzamos por crear un ambiente de confianza y apoyo mutuo.</p>
           </div>
